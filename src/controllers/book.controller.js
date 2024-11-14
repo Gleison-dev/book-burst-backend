f1f52492ad736cfea8d8c35917a923f4c7a347eb
Gleison <gleisonrgomes@gmail.com>
2024-11-14 15:38:09 -0300

    feat: book controller created

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,53 @@
+import { BookService } from "../services/Book.service.js";
+
+const instanceBookService = new BookService();
+
+const createBook = async (req, res) => {
+  const { title, writer, genre, synopsis, year_release } = req.body;
+  const newBook = await instanceBookService.createBookService(
+    title,
+    writer,
+    genre,
+    synopsis,
+    year_release
+  );
+  res.status(201).json({ newBook });
+};
+
+const getAllBooks = async (req, res) => {
+  const books = await instanceBookService.getAllBooks();
+  res.json({ books });
+};
+
+const getBookByTitle = async (req, res) => {
+  const { title } = req.body;
+  const bookByTitle = await instanceBookService.getBookByTitle(title);
+  res.json({ bookByTitle });
+};
+
+const getBookByWriter = async (req, res) => {
+  const { writer } = req.body;
+  const bookByWriter = await instanceBookService.getBookByWriter(writer);
+  res.json({ bookByWriter });
+};
+
+const getBookByGenre = async (req, res) => {
+  const { genre } = req.body;
+  const bookByGenre = await instanceBookService.getBookByGenre(genre);
+  res.json({ bookByGenre });
+};
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  const deletedBook = await instanceBookService.deleteBookService(id);
+  res.json({ deleteBook });
+};
+
+export {
+  createBook,
+  getAllBooks,
+  getBookByGenre,
+  getBookByTitle,
+  getBookByWriter,
+  deleteBook,
+};
