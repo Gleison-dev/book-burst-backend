e488bdb84278359a8f5b2f07379c1c40a0d9bc2e
Gleison <gleisonrgomes@gmail.com>
2024-11-14 16:07:25 -0300

    feat: importing router on server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { testConnection } from "./database/connection.js";
+import { router } from "./routes/index.route.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   testConnection();
