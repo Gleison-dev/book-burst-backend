95ca2fbf97986c737ff2d2f29ccc9c0d07fbe1c9
Gleison <gleisonrgomes@gmail.com>
2024-11-14 08:59:51 -0300

    feat: adding errors message

--- src/shared/book.message.js
+++ src/shared/book.message.js
@@ -1,6 +1,11 @@
+const ERRORS = {
+  ALREADY_EXISTS: "Livro já cadastrado. Tente novamente!",
+  NOT_FOUND: "Livro não encontrado. Tente novamente!"
+}
+
 const SUCESS = {
   CREATED: "Livro cadastrado com sucesso!",
   DELETED: "Livro deletado com sucesso!",
 };
 
-export { SUCESS };
+export { ERRORS, SUCESS };
