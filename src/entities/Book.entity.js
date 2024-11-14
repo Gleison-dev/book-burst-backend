b0867d575ae7adc5d5e3673ed1caa173a80450c5
Gleison <gleisonrgomes@gmail.com>
2024-11-14 08:50:12 -0300

    feat: book entity created

---
+++ src/entities/Book.entity.js
@@ -0,0 +1,30 @@
+import { sequelize as database, sequelize } from "../database/connection.js";
+import { Sequelize, DataTypes } from "sequelize";
+
+const BookEntity = sequelize.define("tb_books", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  writer: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  synopsis: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  year_release: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
