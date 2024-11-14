030e7334d6cedaceea25d47f8ff56915b70cb58a
Gleison <gleisonrgomes@gmail.com>
2024-11-14 16:13:35 -0300

    fix: fixed delete book controller

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -40,7 +40,7 @@ const getBookByGenre = async (req, res) => {
 const deleteBook = async (req, res) => {
   const { id } = req.params;
   const deletedBook = await instanceBookService.deleteBookService(id);
-  res.json({ deleteBook });
+  res.json({ deletedBook });
 };
 
 export {
