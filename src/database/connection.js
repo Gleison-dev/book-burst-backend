a3df0e29bac0522e02affb168a88f69e4a18af14
Gleison <gleisonrgomes@gmail.com>
2024-11-14 08:41:10 -0300

    feat: script of connection to database created

---
+++ src/database/connection.js
@@ -0,0 +1,31 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+  }
+);
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão com o banco de dados realizada!");
+  } catch (error) {
+    return console.error(
+      "Não foi possível conectar com o banco de dados!",
+      error
+    );
+  }
+};
