8fb9568de70b321477a02b28f712fbd839af8359
Gleison <gleisonrgomes@gmail.com>
2024-11-14 09:34:16 -0300

    feat: adding get all books and get book by title

--- src/services/Book.service.js
+++ src/services/Book.service.js
@@ -1,5 +1,5 @@
 import { BookEntity } from "../entities/book.entity.js";
-import { SUCESS } from "../shared/book.message.js";
+import { ERRORS, SUCESS } from "../shared/book.message.js";
 
 class BookService {
   async createBookService(title, writer, genre, synopsis, year_release) {
@@ -22,6 +22,36 @@ class BookService {
         year_release,
       });
       return `${SUCESS.CREATED}`;
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+
+  async getAllBooks() {
+    try {
+      const books = await BookEntity.findAll();
+      return books;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getBookByTitle(title) {
+    try {
+      const verififyBookTitle = await BookEntity.findOne({
+        where: {
+          title,
+        },
+      });
+      if (!verififyBookTitle) {
+        return `${ERRORS.NOT_FOUND}`;
+      }
+      const bookByTitle = await BookEntity.findAll({
+        where: {
+          title,
+        },
+      });
+      return bookByTitle;
     } catch (error) {
       return console.error(error);
     }
